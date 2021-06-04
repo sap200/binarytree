66403c10fc7138906030e2b37861ebf05cb41e1e
sapta <www.saptarsi@gmail.com>
2021-06-05 00:48:29 +0530

    Comments added to node.go

--- node/node.go
+++ node/node.go
@@ -1,16 +1,19 @@
 // package node
 package node
 
+// An interface that ensures that all elements are comparable
 type Comparable interface {
 	CompareTo(e Comparable) int
 }
 
+// The basic node of a tree
 type TreeNode struct {
 	Element   Comparable
 	LeftNode  *TreeNode
 	RightNode *TreeNode
 }
 
+// Create a new node with given element
 func NewTreeNode(e Comparable) *TreeNode {
 	return &TreeNode{
 		Element:   e,
